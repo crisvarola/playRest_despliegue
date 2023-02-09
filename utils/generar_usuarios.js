4942cc481169322a3428a31b5c9075b4a519901a
Cristina Vargas <crisvarola@gmail.com>
2023-02-09 13:47:39 +0100

    generar_usus

--- utils/generar_usuarios.js
+++ utils/generar_usuarios.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Usuario = require(__dirname + '/../models/usuario');
 const bcrypt = require('bcrypt');
-mongoose.connect('mongodb://localhost:27017/playrest_v3');
+mongoose.connect('mongodb://mymongodb/playrest_v3');
 
   Usuario.collection.drop();
   let usu1 = new Usuario({
